aef47268418a43181d03250b3f782f1c40607a49
ihssan <ihssanhadba@gmail.com>
2024-07-14 12:13:18 +0300

    new version

--- script.js
+++ script.js
@@ -1,84 +1,104 @@
 let wordToGuess;
+fetch('data.json')
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Network response was not ok ' + response.statusText);
+        }
+        return response.json();
+    })
+    .then(data => {
+        const words = data.words;
+        const randomIndex = getRandomInt(words.length - 1);
+        const randomWordObject = words[randomIndex];
 
-document.addEventListener('DOMContentLoaded', () => {
-    fetch('data.json')
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('Network response was not ok ' + response.statusText);
-            }
-            return response.json();
-        })
-        .then(data => {
-            const words = data.words;
-            const randomIndex = getRandomInt(0, words.length - 1);
-            const randomWordObject = words[randomIndex];
-
-            console.log(randomWordObject);
-
-            const category = document.getElementById('category');
-            category.textContent = randomWordObject.category;
-            const gameDiv = document.getElementById('word');
-            wordToGuess = randomWordObject.word;
-            for (let i = 0; i < wordToGuess.length; i++) {
-                let wordElement = document.createElement('p');
-                wordElement.textContent = wordToGuess[i];
-                gameDiv.appendChild(wordElement);
-            }
-
-        })
-        .catch(error => {
-            console.error('There has been a problem with your fetch operation:', error);
+        const category = document.getElementById('category');
+        category.textContent = randomWordObject.category;
+        const gameDiv = document.getElementById('word');
+        wordToGuess = randomWordObject.word;
+        //convert wordToGuess from string to array,
+        Array.from(wordToGuess).forEach(element => {
+            let wordElement = document.createElement('p');
+            wordElement.textContent = element;
+            gameDiv.appendChild(wordElement);
         });
-});
-function getRandomInt(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
+        // for (let i = 0; i < wordToGuess.length; i++) {
+        //     let wordElement = document.createElement('p');
+        //     wordElement.textContent = wordToGuess[i];
+        //     gameDiv.appendChild(wordElement);
+        // }
+    })
+    .catch(error => {
+        console.error('There has been a problem with your fetch operation:', error);
+    });
+function getRandomInt(max) {
+    return Math.floor(Math.random() * (max - 0 + 1)) + 0;
 }
 
-const buttons = document.querySelector("#letters");
-let Chance = 0, score=0;
-    buttons.addEventListener("click", (e) => {
-        let correct = 0;
-        if (e.target.textContent.length ===1){
+const buttons = document.querySelectorAll(".btn");
+let Chance = 0, score = 0;
+
+Array.from(buttons).forEach(button => {
+    button.addEventListener("click", (e) => {
+        let correct = false;
         const letterClicked = e.target.textContent;
         let word = document.getElementsByTagName("p");
-            for (let i = 0; i < word.length; i++) {
-               let letter = word[i];
-               if(letter.textContent.toUpperCase() == letterClicked){
-                word[i].style.color = "black";
-                   score++;
-                   correct = 1; 
-               } 
-                if(correct){
-                   e.target.style.background = "green";
-                    if (score == word.length){
-                        const elementToShow = document.querySelector(".result");
-                        const resText = document.querySelector(".resultText");
-                        resText.textContent = ` Congrats!! but you have made ${Chance} mistakes :`;
-                        elementToShow.classList.remove("hide"); 
-                        buttons.classList.add("hide"); 
-                    }} 
-                    else {e.target.style.background = "red"; }   
-                 }
-            if (!correct){
-                Chance++;
-                const elementToShow = document.querySelector(`.line-${Chance}`);
-                elementToShow.classList.remove("hide"); 
-                if (Chance == 11) {
-                    const elementToShow = document.querySelector(".result");
-                    const resText = document.querySelector(".resultText");
-                    resText.textContent = ` Oops! you have lost :( the word is: ${wordToGuess}`;
-                    elementToShow.classList.remove("hide"); 
-                    buttons.classList.add("hide"); 
-                }
+        for (let i = 0; i < word.length; i++) {
+            let letter = word[i];
+            if (letter.textContent == letterClicked.toLowerCase()) {
+                correctAnswer();
             }
-            e.target.setAttribute("disabled", "disabled"); 
+
+            else if (score == word.length) {
+                Winner();
+            }
+
+            function correctAnswer() {
+                word[i].style.color = "black";
+                score++;
+                correct = true;
+                e.target.style.background = "green";
+            };
         }
+
+        if (!correct) {
+            Chance++;
+            e.target.style.background = "red";
+            const elementToShow = document.querySelector(`.line-${Chance}`);
+            elementToShow.classList.remove("hide");
+            if (Chance == 11) {
+                loser();
+            }
+        }
+        e.target.setAttribute("disabled", "disabled");
     }
-);
+    );
+})
+
+
+
+function Winner() {
+    const elementToShow = document.querySelector(".result");
+    const resText = document.querySelector(".resultText");
+    resText.textContent = ` Congrats!! but you have made ${Chance} mistakes :`;
+    elementToShow.classList.remove("hide");
+    buttons.classList.add("hide");
+}
+
+function loser() {
+    const elementToShow = document.querySelector(".result");
+    const resText = document.querySelector(".resultText");
+    resText.textContent = ` Oops! you have lost :( the word is: ${wordToGuess}`;
+    elementToShow.classList.remove("hide");
+    buttons.classList.add("hide");
+}
 
 const hangstandChildren = document.querySelector(".man").children;
 
-for (let i = 0; i < hangstandChildren.length;i++){
-    const child = hangstandChildren[i];
-    child.classList.add("hide");
-}
+Array.from(hangstandChildren).forEach(element => {
+    element.classList.add("hide");
+});
+
+// for (let i = 0; i < hangstandChildren.length;i++){
+//     const child = hangstandChildren[i];
+//     child.classList.add("hide");
+// }
